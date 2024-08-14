ed33a640f6b85fc416a5defe05aa3c58d3c87fff
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-14 17:14:38 +0530

    Added: Part Time Employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -6,7 +6,8 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
         // Declaration
@@ -16,7 +17,11 @@ public class EmployeeWage {
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
         // Condition
-        if(empCheck == IS_FULL_TIME) {
+        if(empCheck == IS_PART_TIME) {
+            System.out.println("Employee is working Part-Time");
+            empHrs = 4;
+        }
+        else if(empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
             empHrs = 8;
         }
