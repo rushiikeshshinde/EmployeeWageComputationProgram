4e5ae13f8ff205fb8915453dbf6e20f285bda8dd
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-14 17:05:49 +0530

    Added: Employee Daily Wage Function

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,6 +7,10 @@ public class EmployeeWage {
 
         // Constants
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        // Declaration
+        int empHrs,empWage;
 
         // Computation
         double empCheck = Math.floor(Math.random() * 10) % 2;
@@ -14,10 +18,16 @@ public class EmployeeWage {
         // Condition
         if(empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
+            empHrs = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+
+        // Calculated Employee Wage
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee wage: " + empWage);
 
     }
 
